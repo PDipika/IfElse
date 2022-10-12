f4d388d6a3c6c4c5c788f1304b5e8dbb0cf4d04b
pdipika <pdipika0713@gmail.com>
2022-10-12 22:27:03 +0100

    StudentResult program

---
+++ src/StudentResult.java
@@ -0,0 +1,32 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class StudentResult {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i =100;
+        double total;
+        float Percentage;
+        System.out.println("Enter total score here : ");
+        total = scanner.nextDouble();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
