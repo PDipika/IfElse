7025962e59871b50d6196f6020aefc183485f430
pdipika <pdipika0713@gmail.com>
2022-10-12 22:02:39 +0100

    Alphabet  program

---
+++ src/AlphabetUcaseLcase.java
@@ -0,0 +1,13 @@
+
+
+public class AlphabetUcaseLcase {
+
+
+    public static void main(String[] args) {
+        String changeCase = "a, b, c, d ";
+        System.out.println( changeCase );
+        String result;
+        result = changeCase.toUpperCase();
+        System.out.println( result);
+    }
+}
