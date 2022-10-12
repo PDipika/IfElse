b51183c00ab796195ceb420fa1d9d0a06569008c
pdipika <pdipika0713@gmail.com>
2022-10-12 22:07:44 +0100

    Average of number program

---
+++ src/AverageOfNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class AverageOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input 1st number");
+        int num1 = scanner.nextInt();
+        System.out.println("input 2nd number");
+        int num2 = scanner.nextInt();
+        System.out.println("input 3Rd number");
+        int num3 = scanner.nextInt();
+        System.out.println("input 4th number");
+        int num4 = scanner.nextInt();
+        System.out.println("input 5th number");
+        int num5 = scanner.nextInt();
+        System.out.println("Average of fivenumbers is : " + (num1 + num2 + num3 +num4 + num5)/5);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
